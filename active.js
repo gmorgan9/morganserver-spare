3a0226b12b84345750a0dbd6dccf96389dac00af
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-29 13:20:39 -0600

    update

---
+++ active.js
@@ -0,0 +1,15 @@
+// Get the container element
+var btnContainer = document.getElementById("nav");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("nav-item-link");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    console.log('clicked');
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
