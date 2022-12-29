210bdb27b68ff4e24b11059a3e85e65f5a02c160
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-29 13:27:55 -0600

    update

--- active.js
+++ active.js
@@ -23,9 +23,11 @@
   
 //   });
 
-  $(document).ready(function() {
-    $(document).on('click', '.menu',function () {
-        $('.nav-item-link').removeClass(" active");
-        $(this).addClass(" active");
-    });
+var btns = document.querySelectorAll(".nav-item-link");
+Array.from(btns).forEach(item => {
+   item.addEventListener("click", () => {
+      var selected = document.getElementsByClassName("active");
+      selected[0].className = selected[0].className.replace(" active", "");
+      item.className += " active";
+   });
 });
